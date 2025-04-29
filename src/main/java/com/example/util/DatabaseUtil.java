42ef6598f3f2c7c8c2241300100c65c264f81677
PavloSuprun <73907408+PavloSuprun@users.noreply.github.com>
2025-04-29 13:58:45 +0300

    Update DatabaseUtil.java
    Rewrite connection string in one like for easier parsing

--- src/main/java/com/example/util/DatabaseUtil.java
+++ src/main/java/com/example/util/DatabaseUtil.java
@@ -5,15 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtil {
-    private static final String JDBC_URL = 
-    "jdbc:sqlserver://pavsupr-sql-server-failover-group.database.windows.net:1433;" +
-    "database=pavsuprdb;" +
-    "user=pavsupr@pavsupr-sql-server;" +
-    "password=password;" +
-    "encrypt=true;" +
-    "trustServerCertificate=false;" +
-    "hostNameInCertificate=*.database.windows.net;" +
-    "loginTimeout=30;";
+    private static final String JDBC_URL = "connection_string";
 
     public static Connection getConnection() throws SQLException {
         try {
